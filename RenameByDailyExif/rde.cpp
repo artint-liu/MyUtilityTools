d08cdadf96ed51d6039b2395a903c9a9044ab3a2
stdliu <artint.liu@gmail.com>
2024-08-23 18:27:59 +0800

    *整理代码

--- RenameByDailyExif/rde.cpp
+++ RenameByDailyExif/rde.cpp
@@ -5,175 +5,17 @@
 #include "clstd.h"
 #include "clString.h"
 #include "clPathFile.h"
-//#include "FreeImage.h"
-#include "libheif\heif_cxx.h"
 #include "shlobj.h"
 #include <iostream>
 #include <wincodec.h>
 
-//#ifdef _DEBUG
-//# pragma comment(lib, "FreeImaged.lib")
-//#else
-//# pragma comment(lib, "FreeImageLib.lib")
-//#endif
-//
-//#pragma comment(lib, "heif.lib")
 #pragma comment(lib, "shell32.lib")
 IWICImagingFactory* pFactory = nullptr;
 
-//void PrintMetadata(const char *sectionTitle, FIBITMAP *dib, FREE_IMAGE_MDMODEL model)
-//{
-//  FITAG *tag = NULL;
-//  FIMETADATA *mdhandle = NULL;
-//
-//  mdhandle = FreeImage_FindFirstMetadata(model, dib, &tag);
-//
-//  if(mdhandle) {
-//    // Print a table section
-//    //PrintTableSection(ios, sectionTitle);
-//
-//    do {
-//      // convert the tag value to a string
-//      const char *value = FreeImage_TagToString(model, tag);
-//
-//      // print the tag 
-//      // note that most tags do not have a description, 
-//      // especially when the metadata specifications are not available
-//      if(FreeImage_GetTagDescription(tag)) {
-//        std::cout << "<TR><TD>" << FreeImage_GetTagKey(tag) << "</TD><TD>" << value << "</TD><TD>" << FreeImage_GetTagDescription(tag) << "</TD></TR>\n";
-//      }
-//      else {
-//        std::cout << "<TR><TD>" << FreeImage_GetTagKey(tag) << "</TD><TD>" << value << "</TD><TD>" << "&nbsp;" << "</TD></TR>\n";
-//      }
-//
-//    } while(FreeImage_FindNextMetadata(mdhandle, &tag));
-//  }
-//
-//  FreeImage_FindCloseMetadata(mdhandle);
-//}
-
-#define CHECK_HEIF_RESULT  if (err.code != heif_error_Ok) { CLOG_ERROR("heif error: %d, %d(%s)", err.code, err.subcode, err.message); break; }
-
-
-struct ExifTag{
-    int tagId;
-    int dataType;
-    int numComponents;
-    int valueOffset;
-};
-
-std::string extractDateTimeFromExif(unsigned char* exifData, size_t length) {
-    int numTags = (exifData[4] << 8) | exifData[5];
-    int currentOffset = 6;
-
-    std::string dateTime = "拍摄日期未找到";
-
-    for (int i = 0; i < numTags; ++i) {
-        ExifTag tag;
-        tag.tagId = (exifData[currentOffset] << 8) | exifData[currentOffset + 1];
-        currentOffset += 2;
-        tag.dataType = (exifData[currentOffset] << 8) | exifData[currentOffset + 1];
-        currentOffset += 2;
-        tag.numComponents = (exifData[currentOffset] << 24) | (exifData[currentOffset + 1] << 16) | (exifData[currentOffset + 2] << 8) | exifData[currentOffset + 3];
-        currentOffset += 4;
-        tag.valueOffset = (exifData[currentOffset] << 24) | (exifData[currentOffset + 1] << 16) | (exifData[currentOffset + 2] << 8) | exifData[currentOffset + 3];
-        currentOffset += 4;
-
-        if (tag.tagId == 0x9003) { // DateTimeOriginal 标签 ID
-            if (tag.dataType == 2) { // ASCII string
-                int stringOffset = tag.valueOffset;
-                int year = (exifData[stringOffset] << 8) | exifData[stringOffset + 1];
-                stringOffset += 2;
-                int month = exifData[stringOffset];
-                stringOffset++;
-                int day = exifData[stringOffset];
-                stringOffset++;
-                int hour = exifData[stringOffset];
-                stringOffset++;
-                int minute = exifData[stringOffset];
-                stringOffset++;
-                int second = exifData[stringOffset];
-                stringOffset++;
-
-                dateTime = std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day) + " " +
-                    std::to_string(hour) + ":" + std::to_string(minute) + ":" + std::to_string(second);
-                break;
-            }
-        }
-    }
-
-    return dateTime;
-}
-
-//BOOL HEIFReadDateTimeFromFile(clvector<clStringA>& sTimes, LPCWSTR szFilename)
-//{
-//    BOOL result = FALSE;
-//    uint8_t* exifData = nullptr;
-//    size_t exifSize;
-//    do
-//    {
-//        heif_context* ctx = heif_context_alloc();
-//        clstd::Buffer buffer;
-//        clstd::File file;
-//        if (!file.OpenExisting(szFilename))
-//        {
-//            CLOG_ERROR("无法打开文件");
-//            break;
-//        }
-//
-//        if (!file.Read(&buffer))
-//        {
-//            CLOG_ERROR("无法读取文件");
-//            break;
-//        }
-//        
-//        heif_error err = heif_context_read_from_memory(ctx, buffer.GetPtr(), buffer.GetSize(), nullptr);
-//        CHECK_HEIF_RESULT;
-//
-//        // get a handle to the primary image
-//        heif_image_handle* handle;
-//        err = heif_context_get_primary_image_handle(ctx, &handle);
-//        CHECK_HEIF_RESULT;
-//
-//        // decode the image and convert colorspace to RGB, saved as 24bit interleaved
-//        //heif_image* img;
-//        //err = heif_decode_image(handle, &img, heif_colorspace_RGB, heif_chroma_interleaved_RGB, nullptr);
-//        //CHECK_HEIF_RESULT;
-//
-//        heif_item_id exif_id;
-//
-//        int n = heif_image_handle_get_list_of_metadata_block_IDs(handle, "Exif", &exif_id, 1);
-//        if (n == 1) {
-//            exifSize = heif_image_handle_get_metadata_size(handle, exif_id);
-//            exifData = new uint8_t[exifSize];
-//            err = heif_image_handle_get_metadata(handle, exif_id, exifData);
-//            CHECK_HEIF_RESULT;
-//        }
-//
-//        extractDateTimeFromExif(exifData, exifSize);
-//
-//        //FITAG tag = { exifData };
-//        //clStringA str = (const char*)FreeImage_GetTagValue(&tag);
-//
-//        //int stride;
-//        //const uint8_t* data = heif_image_get_plane_readonly(img, heif_channel_interleaved, &stride);
-//
-//        // ... process data as needed ...
-//
-//        // clean up resources
-//        //heif_image_release(img);
-//        heif_image_handle_release(handle);
-//        heif_context_free(ctx);
-//
-//        result = TRUE;
-//    } while (false);
-//
-//    SAFE_DELETE_ARRAY(exifData);
-//    return result;
-//}
 
 BOOL ReadDateTimeFromFile(clvector<clStringA>& sTimes, LPCWSTR szFilename)
 {
+    // 参考文档：
     // https://learn.microsoft.com/zh-cn/windows/win32/wic/-wic-codec-readingwritingmetadata
     // https://learn.microsoft.com/zh-cn/windows/win32/wic/-wic-native-image-format-metadata-queries#exif-metadata
     
@@ -247,23 +89,6 @@ BOOL ReadDateTimeFromFile(clvector<clStringA>& sTimes, LPCWSTR szFilename)
             }
         }
 
-        //// 查询 EXIF 信息
-        //PROPVARIANT value;
-        //PropVariantInit(&value);
-        ////LPCWSTR name = L"/app1/ifd/PaddingSchema:padding";
-        ////LPCWSTR name = L"/ifd/exif/{ushort=1111}";
-        //LPCWSTR name = L"/app1/ifd/exif/{ushort=40962}";
-        ////LPCWSTR name = L"/app1/ifd/exif";
-        ////LPCWSTR name = L"/app1/ifd/{ushort=34665}";
-        //hr = pQueryReader->GetMetadataByName(name, &value);
-
-        //if (SUCCEEDED(hr)) {
-        //    std::wcout << L"EXIF Information: " << value.bstrVal << std::endl;
-        //    PropVariantClear(&value);
-        //}
-        //else {
-        //    std::cerr << "Failed to get EXIF information. Error: " << hr << std::endl;
-        //}
     } while (false);
 
     // 清理资源
@@ -275,63 +100,6 @@ BOOL ReadDateTimeFromFile(clvector<clStringA>& sTimes, LPCWSTR szFilename)
     return sTimes.size() == 6;
 }
 
-//BOOL FTReadDateTimeFromFile(clvector<clStringA>& sTimes, LPCWSTR szFilename)
-//{
-//    sTimes.clear();
-//    FIBITMAP* bmp = NULL;
-//    BOOL result = FALSE;
-//    do {
-//        FREE_IMAGE_FORMAT fmt = FreeImage_GetFIFFromFilenameU(szFilename);
-//        if (fmt == FIF_UNKNOWN) {
-//            CLOG_ERROR("不是jpg文件");
-//            break;
-//        }
-//
-//        if (bmp) {
-//            FreeImage_Unload(bmp);
-//            bmp = NULL;
-//        }
-//
-//        bmp = FreeImage_LoadU(fmt, (const wchar_t*)(szFilename));
-//        if (bmp == NULL) {
-//            CLOG_ERROR("无法读取文件");
-//            break;
-//        }
-//
-//        FITAG* tag = NULL;
-//        if (FreeImage_GetMetadata(FIMD_EXIF_MAIN, bmp, "DateTime", &tag) == FALSE) {
-//            CLOG_ERROR("EXIF中没有拍摄日期");
-//            break;
-//        }
-//
-//        clStringA str = (const char*)FreeImage_GetTagValue(tag);
-//        if (str.IsEmpty()) {
-//            CLOG_ERROR("EXIF中拍摄日期为空");
-//            break;
-//        }
-//        str.Replace(' ', ':');
-//        //clvector<clStringA> sTimes;
-//        clstd::StringUtility::Resolve(str, ':', [&sTimes](size_t index, const ch* str, size_t len)
-//            {
-//                clStringA strTemp;
-//                strTemp.Append(str, len);
-//                sTimes.push_back(strTemp);
-//            });
-//
-//        if (sTimes.size() != 6) {
-//            CLOG_ERROR("EXIF中拍摄日期格式不正确");
-//            break;
-//        }
-//        result = TRUE;
-//    } while (false);
-//
-//    if (bmp)
-//    {
-//        FreeImage_Unload(bmp);
-//        bmp = NULL;
-//    }
-//    return result;
-//}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -389,7 +157,6 @@ int _tmain(int argc, _TCHAR* argv[])
                 continue;
             }
         }
-        //PrintMetadata("FIMD_EXIF_MAIN", bmp, FIMD_EXIF_MAIN);
         SYSTEMTIME time = { 0 };
         time.wYear = sTimes[0].ToInteger();
         time.wMonth = sTimes[1].ToInteger();
@@ -422,7 +189,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
     pFactory->Release();
     CoUninitialize();
-    //FreeImage_DeInitialise();
   }
 	return 0;
 }
