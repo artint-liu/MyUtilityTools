70ea50b32aaec0b5f1d378208e45347f3c8e32d3
artint <artint.liu@gmail.com>
2025-02-22 22:54:30 +0800

    *修改源码路径，原来自动生成的sln和vcxproj中使用的是绝对路径，改为相对路径

--- astcenc/Source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/codes/PMGame/PMGameToolSrc/astc-encoder/Source
-# Build directory: E:/codes/PMGame/PMGameToolSrc/astc-encoder/Source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
